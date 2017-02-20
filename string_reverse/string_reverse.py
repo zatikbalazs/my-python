9e544fddd981623b59ab371b5908ca08c193c8b2
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-02-19 19:11:54 -0500

    Create string reverse function

---
+++ string_reverse/string_reverse.py
@@ -0,0 +1,20 @@
+def reverse_string(string):
+    """
+    Reverse any string.
+    args: str(string)
+    return: str(reversed_string)
+    """
+    # Reverse the string.
+    reversed_string = string[::-1]
+
+    return reversed_string
+
+
+# User enters string.
+string = input("Enter the text you want to reverse: ").strip()
+
+# Call the function.
+reversed_string = reverse_string(string)
+
+# Print reversed string.
+print(reversed_string)
