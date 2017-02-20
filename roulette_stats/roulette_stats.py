5e0936d0866624c7271a593518a91715bc579672
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-02-20 12:03:06 -0500

    Add stats for even, odd and zero hits

--- roulette_stats/roulette_stats.py
+++ roulette_stats/roulette_stats.py
@@ -14,14 +14,36 @@ numbers = [number for number in range(37)]
 # How many spins?
 spins = int(input("How many spins? ").strip())
 
+# Initialize variables for stats.
+even_hits = 0
+zero_hits = 0
+odd_hits  = 0
+
 # Start spins.
 for spin in range(spins):
+
+    # Current number of spin.
     spin_num = spin + 1
+
+    # Get random hit.
     hit = random.choice(numbers)
+
+    # Even, odd or zero?
+    if hit == 0:
+        zero_hits += 1
+    elif hit % 2 == 0:
+        even_hits += 1
+    else:
+        odd_hits += 1
+
+    # Display result of current spin.
     print("{}. spin: {}".format(spin_num, hit))
 
-# Statistics.
+# Display statistics.
 print("=" * 10)
 print("Statistics".upper())
 print("=" * 10)
-print("Spins: {}".format(spin_num))
+print("Spins:     {}".format(spins))
+print("Even hits: {}".format(even_hits))
+print("Odd hits:  {}".format(odd_hits))
+print("Zero hits: {}".format(zero_hits))
