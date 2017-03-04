5c6fc1819b0ce0fe5cae851a73852fd571d06a86
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-03-04 12:24:57 -0500

    Learn about params, default params, args and kwargs

---
+++ params_args/params_args.py
@@ -0,0 +1,23 @@
+"""
+Learn about parameters, default parameters,
+arguments and keyword arguments.
+"""
+
+# PARAMETERS of the function.
+def add(a, b):
+    return a + b
+
+# Pass ARGUMENTS to the function.
+add(1, 2)
+
+
+# Default parameters.
+# WARNING! Default params must always go after "normal" params!
+def about(name, age, hobby = "Python"):
+    sentence = "Name: {}. Age: {}. Hobby: {}.".format(name, age, hobby)
+    return sentence
+
+about("Tom", 30)
+
+# Keyword arguments.
+about(age = 40, name = "Jennifer")
