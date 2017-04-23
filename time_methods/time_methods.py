6b948662ad9ef1b7ad6d3cda98596d8ec404f298
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-04-23 15:14:35 -0400

    Learn more time methods

--- time_methods/time_methods.py
+++ time_methods/time_methods.py
@@ -1,5 +1,5 @@
 """
-Time methods in Python.
+The most common time methods in Python.
 """
 
 # Import module.
@@ -15,4 +15,23 @@ print(output)
 
 # Formatted local time.
 output = time.asctime(time.localtime(time.time()))
+print(output)
+
+# GMT.
+output = time.gmtime(time.time())
+print(output)
+
+# Sleep for "n" seconds.
+time.sleep(1)
+
+# Time of current process.
+output = time.process_time()
+print(output)
+
+# Format time.
+output = time.strftime("%Y.%m.%d. %H:%M:%S")
+print(output)
+
+# Parse time.
+output = time.strptime("30 Nov 00", "%d %b %y")
 print(output)
