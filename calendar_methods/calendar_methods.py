20a01bd169d5aeefe8fefd376a79ff8a4f35874d
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-04-23 23:09:30 -0400

    Learn more calendar functions

--- calendar_methods/calendar_methods.py
+++ calendar_methods/calendar_methods.py
@@ -8,3 +8,18 @@ import calendar
 # Show calendar.
 cal = calendar.month(1985, 10)
 print(cal)
+
+# Get first weekday.
+output = calendar.firstweekday()
+print(output)
+
+# Set first weekday.
+output = calendar.setfirstweekday(calendar.SUNDAY)
+
+# Get day of the week.
+output = calendar.weekday(1985, 10, 2)
+print(output)
+
+# Is leap year?
+output = calendar.isleap(2017)
+print(output)
