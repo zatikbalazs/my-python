25443cd60ccc75590aa6e15050f4b9fe2e1cc5bb
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-05-28 21:39:12 +0200

    Create new project: write text file

---
+++ write_text_file/write_text_file.py
@@ -0,0 +1,28 @@
+"""
+Write to a text file.
+
+Step 1: open file with open().
+Step 2: write to file with write().
+Step 3: close file with close().
+"""
+
+# Open file for writing.
+file = open("example.txt", "w") # Use "a" to append to the end of file.
+
+# Create an empty list of names.
+names = []
+
+# User has to enter 3 names.
+while len(names) <= 2:
+    name = input("Enter a name: ").strip().title()
+    names.append(name)
+
+print("Saving names... {}".format(names))
+
+# Write to file (as a string).
+file.write(str(names))
+
+# Close file.
+file.close()
+
+print("Names saved!")
