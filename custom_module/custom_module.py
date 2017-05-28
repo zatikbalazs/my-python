d8ba2f5ff4632e0e19979fde2c3a65daa98d894f
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-05-28 13:45:04 +0200

    Learn how to create and use my own modules

---
+++ custom_module/custom_module.py
@@ -0,0 +1,12 @@
+"""
+This file demonstrates the usage of a custom module.
+"""
+
+# Import the custom module.
+# It has to be in the same directory as the file running.
+import my_module
+
+# Using the module.
+title = my_module.header("This is The Title")
+
+print(title)
---
+++ custom_module/my_module.py
@@ -0,0 +1,13 @@
+def header(title):
+    """
+    Create a nice header.
+    params: str(title)
+    return: str(header)
+    """
+    # Get the length of title.
+    length = len(title)
+
+    # Concatenate header string.
+    header = "=" * length + "\n" + title.upper() + "\n" + "=" * length
+
+    return header
