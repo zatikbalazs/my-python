8578adf577d595fe7095ae72040f290ac416a145
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-05-28 16:54:06 +0200

    Add unit option to Temperature Converter

--- temperature_converter/temperature_converter.py
+++ temperature_converter/temperature_converter.py
@@ -3,11 +3,6 @@ Coding Exercise: Functions
 
 Write a function that will convert either a Celsius temperature
 to a Fahrenheit temperature or a Fahrenheit temperature to Celsius.
-
-You'll need to have your function receive two parameters.
-You might want to make the first parameter the temperature
-to convert, and the second temperature a boolean indicating
-whether the value sent is Celsius or Fahrenheit.
 """
 
 def header(title):
@@ -25,13 +20,15 @@ def header(title):
     return header
 
 
-def convert_temp(temp, celsius):
+def convert_temp(unit, temp):
     """
     Convert either a Celsius temperature to a Fahrenheit
     temperature or a Fahrenheit temperature to Celsius.
-    params: float(temp), bool(celsius)
+    params: str(unit), float(temp)
     return: float(result)
     """
+
+    return result
 
 
 # Print header.
@@ -39,9 +36,24 @@ print(header("Temperature Converter"))
 
 # User input.
 while True:
+    unit = input("Celsius or Fahrenheit (C/F)? ").strip().lower()
+
+    if unit == "c" or unit == "f":
+        break
+    else:
+        print("Invalid unit: \"{}\"".format(unit))
+        continue
+
+while True:
     try:
         temp = float(input("Enter temperature: ").strip())
         break
     except ValueError:
         print("Only numbers are accepted! Correct format: 25.00")
         continue
+
+# Call the converter function.
+result = convert_temp(unit, temp)
+
+# Print the rounded result.
+print("{} = {}".format(temp, result))
