0eadbcf02c5b50727b505987a4f221c0ae7f1285
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-05-28 21:09:40 +0200

    Create new project: read text file

---
+++ read_text_file/read_text_file.py
@@ -0,0 +1,12 @@
+"""
+Read content of a text file.
+
+Step 1: open file with open().
+Step 2: read content with read().
+"""
+
+# Open file.
+file = open("example.txt")
+
+# Read and print the file's content to console.
+print(file.read())
