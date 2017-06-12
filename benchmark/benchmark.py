5c658de980d38d6de42a76e8371565085cb39915
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-06-12 15:07:09 +0200

    Option to run several benchmarks

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -10,34 +10,43 @@ def divide(a, b):
     return a / b
 
 # Set benchmark difficulty.
-difficulty = 5000
+difficulty = 2000
 diff_short = round(difficulty / 1000)
 
-# Set divident.
-divident = 1
+# Set number of tests.
+test_count = 3
+test_num = 1
 
 # Benchmark starts.
 print("{}K benchmark in progress... Please wait.".format(diff_short))
 
-# Get starting time.
-start_time = time.time()
+# Start the cycle of tests.
+while test_num <= test_count:
+    # Set divident.
+    divident = 1
 
-# Start main loop based on difficulty.
-while divident <= difficulty:
-    # Set divisor.
-    divisor = 1
+    # Get starting time.
+    start_time = time.time()
 
-    while divisor <= divident:
-        divide(divident, divisor)
+    # Start main loop based on difficulty.
+    while divident <= difficulty:
+        # Set divisor.
+        divisor = 1
 
-        # Increase divisor.
-        divisor += 1
+        while divisor <= divident:
+            divide(divident, divisor)
 
-    # Increase divident.
-    divident += 1
+            # Increase divisor.
+            divisor += 1
 
-# Calculate and round execution time.
-exec_time = round(time.time() - start_time)
+        # Increase divident.
+        divident += 1
 
-# Print result.
-print("{}K benchmark result: {} s".format(diff_short, exec_time))
+    # Calculate and round execution time.
+    exec_time = round(time.time() - start_time)
+
+    # Print result.
+    print("{}K benchmark result #{}: {} s".format(diff_short, test_num, exec_time))
+
+    # Increase test number.
+    test_num += 1
