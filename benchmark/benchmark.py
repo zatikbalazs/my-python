ed0690ea26099e2aacc74a61ca9510ae4076bbf7
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-06-12 16:51:17 +0200

    Improve benchmarking tool for better results

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -10,11 +10,13 @@ def divide(a, b):
     return a / b
 
 # Set benchmark difficulty.
-difficulty = 2000
+difficulty = 5000
 diff_short = round(difficulty / 1000)
 
 # Set number of tests.
-test_count = 3
+test_count = 5
+
+# Initialize test number.
 test_num = 1
 
 # Benchmark starts.
@@ -43,10 +45,14 @@ while test_num <= test_count:
         divident += 1
 
     # Calculate and round execution time.
-    exec_time = round(time.time() - start_time)
+    exec_time = round(time.time() - start_time, 2)
 
     # Print result.
-    print("{}K benchmark result #{}: {} s".format(diff_short, test_num, exec_time))
+    print("Result #{}: {} s".format(test_num, exec_time))
 
     # Increase test number.
     test_num += 1
+
+    # Sleep between testing cycles (skip the last one).
+    if test_num < 4:
+        time.sleep(5)
