e0032af138a0508108ec6102ba4aad4d6340810b
dr. Balazs Zatik <zatikbalazs@gmail.com>
2017-06-13 12:02:31 +0200

    Learn more about banker's rounding

--- rounding/rounding.py
+++ rounding/rounding.py
@@ -8,7 +8,19 @@ number = math.pi # 3.141592653589793
 
 # ---------------------------------
 # Using the inbuilt round() method.
+# Be aware that this method uses
+# the so called banker's rounding!
 # ---------------------------------
+"""
+What is banker's rounding?
+--------------------------
+.5 will be rounded to the nearest
+EVEN integer, or 0. Examples:
+0.5 => 0
+1.5 => 2
+2.5 => 2
+3.5 => 4
+"""
 
 # Round to nearest integer.
 result = round(number)
